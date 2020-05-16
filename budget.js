0d9efad95f2c54118f76aef700c25138091f924b
Kseniya <ad50depo@yandex.ru>
2020-05-16 16:45:22 +0300

    step 3

--- budget.js
+++ budget.js
@@ -1,136 +1,88 @@
-<<<<<<< HEAD
+'use strict';
+
+
 let money, time;
 
 function start() {
-	money = +prompt("Ваш бюджет на месяц?", '');
-=======
-let money = +prompt("Ваш бюджет на месяц?", ''),
->>>>>>> master
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
 
-	while(isNaN(money) || money == "" || money == null) {
-		money = +prompt("Ваш бюджет на месяц?", '');
-	}
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", ""); 
+    }
+
 }
 start();
 
+    
 let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: true
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
 };
 
-<<<<<<< HEAD
+
 function chooseExpenses() {
-	for (let i = 1; i <= 1; i++) {
-		 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-		 		b = prompt("Во сколько обойдется?", '');
-		 	if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-		 	    && a != '' && b != '' && a.length < 50)  {
-		 	    console.log('Done!');
-		 		appData.expenses[a] = b;
-		 	}	else {
-		 		console.log('Что-то пошло не так');
-		 		i--;
-		 	}
-		 }
+    for (let i = 0; i < 2; i++) {
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt ("Во сколько обойдется?", "");
+    
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+    
+    };
 }
 chooseExpenses();
 
-function detectDayBudget() {
-appData.moneyPerDay = (appData.budget / 30).toFixed();
-alert('Ежедневный бюджет: ' + appData.moneyPerDay + "руб.");
+
+function detectDayBudget() {                                            // Расчет дневного бюджета
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
 }
 detectDayBudget();
 
-function detectLevel() {
-if (appData.moneyPerDay < 100) {
-	console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-	console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-	console.log("Высокий уровень достатка");
-} else {
-	console.log("Произошла ошибка");
-}
+
+function detectLevel() {                                                // Расчет уровня достатка
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Ошибочка...!");
+    }
 }
 detectLevel();
 
+
+
 function checkSavings() {
-     if (appData.savings == true) {
-		 let save = +prompt("Какова сумма накоплений?"),
-			 percent = +prompt("Под какой процент?");
-			 
-	appData.monthIncome = (save/100/12*percent).toFixed();
-	alert("Доход в месяц с Вашего депозита: " + appData.monthIncome);
-	 }
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+            appData.monthIncome = save/100/12*percent;
+            alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+    }
 }
 checkSavings();
 
-function chooseOptExpenses() {
-	for (i=1; i<=3; i++) {
-	let questionOptExpenses = prompt("Статья необязательных расходов")
-	appData.optionalExpenses[i] = questionOptExpenses;
-	console.log(appData.optionalExpenses);
-	}
+
+function chooseOptExpenses() {                             // Функция для определения необязательных расходов
+
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
+
+
 }
 chooseOptExpenses();
-=======
-for (let i = 0; i < 2; i++) {
-	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-		b = prompt("Во сколько обойдется?", '');
-	if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-	    && a != '' && b != '' && a.length < 50)  {
-	    console.log('Done!');
-		appData.expenses[a] = b;
-	}	else {
-		console.log('Что-то пошло не так');
-		i--;
-	}
-};
-
-//let i = 0;
-//while (i < 2) {
-	//let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-	      //   b = prompt ("Во сколько обойдется?", "");
-	    // if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-	    //    console.log ("done");
-	      //   appData.expenses[a] = b;
-	    // } else {
-	     //     console.log ("bad result");
-	     //     i--;
-	    // }
-	   //  i++;
-//}
-
-//let i = 0;
-//do {
-//	let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//	b = prompt ("Во сколько обойдется?", "");
-//if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-//   console.log ("done");
-//	appData.expenses[a] = b;
-//} else {
-//	 console.log ("bad result");
-//	 i--;
-//}
-//i++;
-//} while (i < 2)
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert('Ежедневный бюджет: ' + appData.moneyPerDay + "руб.");
-
-if (appData.moneyPerDay < 100) {
-	console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-	console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-	console.log("Высокий уровень достатка");
-} else {
-	console.log("Произошла ошибка");
-}
->>>>>>> master
